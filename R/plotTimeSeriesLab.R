4eb7da5a2bdefa217972d7a89fd17429cfefdaf5
ChunJuC <ju.chin.0902@gmail.com>
2020-09-24 22:45:17 +0800

    Update function

--- R/plotTimeSeriesLab.R
+++ R/plotTimeSeriesLab.R
@@ -5,15 +5,16 @@ plotTimeSeriesLab <- function(labData, idColName, labItemColName, timeMarkColNam
 
   labData <- as.data.table(labData)
   setnames(labData,deparse(substitute(idColName)), "ID")
-  setnames(labData,deparse(substitute(labItemColName)), "LAB")
+  labCols <- unlist(strsplit(deparse(substitute(labItemColName))," [+] "))
+
   setnames(labData,deparse(substitute(timeMarkColName)), "TimeMark")
   setnames(labData,deparse(substitute(valueColName)), "Value")
   if(deparse(substitute(abnormalMarkColName)) != "NULL"){
     setnames(labData,deparse(substitute(abnormalMarkColName)), "abnormalMark")
-    labData <- labData[,c("ID", "LAB", "TimeMark", "Value", "abnormalMark"), with = FALSE]
+    labData <- labData[,c("ID", labCols, "TimeMark", "Value", "abnormalMark"), with = FALSE]
     labData[, abnormalMark := ifelse(is.na(abnormalMark), "Normal", abnormalMark)]
   }else{
-    labData <- labData[,c("ID", "LAB", "TimeMark", "Value"), with = FALSE]
+    labData <- labData[,c("ID", labCols, "TimeMark", "Value"), with = FALSE]
   }
   if(!is.null(timeStart)){
     labData <- labData[TimeMark >= timeStart]
@@ -26,9 +27,9 @@ plotTimeSeriesLab <- function(labData, idColName, labItemColName, timeMarkColNam
   labData$Value <- as.numeric(labData$Value)
 
   if(deparse(substitute(abnormalMarkColName)) != "NULL"){
-    plot <- ggplot(labData, aes(x = TimeMark , y = Value, group = ID , col = ID)) + geom_line(size = 1) + geom_point(aes(shape = abnormalMark), size = 3) + scale_shape_manual(values=c(17, 25, 16)) + scale_y_continuous() + facet_wrap( ~ LAB, scales = "free") #+ geom_line(group =2)
+    plot <- ggplot(labData, aes(x = TimeMark , y = Value, group = ID , col = ID)) + geom_line(size = 1) + geom_point(aes(shape = abnormalMark), size = 3) + scale_shape_manual(values=c(17, 25, 16)) + scale_y_continuous() + facet_wrap( ~  deparse(substitute(labItemColName)), scales = "free") #+ geom_line(group =2)
   }else{
-    plot <- ggplot(labData, aes(x = TimeMark , y = Value, group = ID , col = ID)) + geom_line(size = 1) + geom_point() + scale_y_continuous() + facet_wrap( ~ LAB, scales = "free") #+ geom_line(group =2)
+    plot <- ggplot(labData, aes(x = TimeMark , y = Value, group = ID , col = ID)) + geom_line(size = 1) + geom_point() + scale_y_continuous() + facet_wrap( ~  deparse(substitute(labItemColName)), scales = "free") #+ geom_line(group =2)
   }
   return(plot)
 }
